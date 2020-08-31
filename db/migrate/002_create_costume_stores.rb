8f3faa4b433035e94983e71941c344115a9ec369
Daniel Gould <dmgould310@gmail.com>
2020-08-31 18:16:28 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,19 @@
 # Create your costume_stores migration here
+
+class CostumeStores < ActiveRecord::Migration[4.2]
+  
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :still_in_business
+      t.datetime :closing_time 
+    end 
+  end 
+    
+  
+  
+  
+end 
